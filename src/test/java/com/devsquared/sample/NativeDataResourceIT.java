49b700d23fff5bc0bb886e46898a162be25a6a05
Devin Ward <devinw1325wc@gmail.com>
2019-03-27 22:32:11 -0500

    Add files via upload

---
+++ src/test/java/com/devsquared/sample/NativeDataResourceIT.java
@@ -0,0 +1,9 @@
+package com.devsquared.sample;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeDataResourceIT extends DataResourceTest {
+
+    // Execute the same tests but in native mode.
+}
